1544eea0ddfd815a020a4d41a09ab4f557d21686
Rahul Pamnani <rahulpamnani6603@gmail.com>
2024-12-06 20:06:07 +0530

    Optimize Day06 solutions

--- src/Day06.kt
+++ src/Day06.kt
@@ -4,11 +4,13 @@ fun main() {
         ^   Initial position
         .   Empty space
         #   Unvisited obstacle
-        U   Obstacle visited moving up
-        R   Obstacle visited moving right
-        D   Obstacle visited moving down
-        L   Obstacle visited moving left
-     */
+        0   Obstacle visited moving up
+        1   Obstacle visited moving right
+        2   Obstacle visited moving down
+        3   Obstacle visited moving left
+    */
+
+    val directionList = listOf(-1 to 0, 0 to 1, 1 to 0, 0 to -1) // Up, Right, Down, Left
 
     fun findInitialPosition(input: List<MutableList<Char>>): Pair<Int, Int> {
         for (row in input.indices) {
@@ -21,80 +23,56 @@ fun main() {
         return Pair(-1, -1)
     }
 
-    fun findNextPosition(input: List<MutableList<Char>>, currentPosition: Pair<Int, Int>): Pair<Int, Int> {
+    tailrec fun traverseGrid(
+        input: List<MutableList<Char>>,
+        currentPosition: Pair<Int, Int>,
+        currentDirectionIdx: Int
+    ): Boolean {
         val (currentRow, currentColumn) = currentPosition
-        for (rowNumber in currentRow downTo 1) {
-            input[rowNumber][currentColumn] = 'X'
-            if (input[rowNumber - 1][currentColumn] == '#' ||
-                input[rowNumber - 1][currentColumn] in listOf('U', 'R', 'D', 'L') // For Part 2
-            ) {
-                return Pair(rowNumber, currentColumn)
-            }
+        input[currentRow][currentColumn] = 'X'
+        if (currentRow == 0 || currentColumn == 0 || currentRow == input.size - 1 || currentColumn == input[0].size - 1) {
+            return false
         }
-        input[0][currentColumn] = 'X'
-        return Pair(0, currentColumn)
-    }
-
-    // Rotate the input 90 degrees anti-clockwise
-    fun rotateInput(input: List<MutableList<Char>>): List<MutableList<Char>> {
-        val numRows = input.size
-        val numCols = input[0].size
-
-        val rotatedInput = MutableList(numCols) { mutableListOf<Char>() }
-
-        for (col in numCols - 1 downTo 0) {
-            for (row in 0..<numRows) {
-                rotatedInput[numCols - col - 1].add(input[row][col])
+        val currentDirection = directionList[currentDirectionIdx]
+        val (nextRow, nextColumn) = Pair(currentRow + currentDirection.first, currentColumn + currentDirection.second)
+        if (input[nextRow][nextColumn] == '#' || input[nextRow][nextColumn] in listOf('0', '1', '2', '3')) {
+            if (input[nextRow][nextColumn] == currentDirectionIdx.digitToChar()) {
+                return true
             }
+            if (input[nextRow][nextColumn] == '#') {
+                input[nextRow][nextColumn] = currentDirectionIdx.digitToChar()
+            }
+            val nextDirectionIdx = (currentDirectionIdx + 1) % 4
+            return traverseGrid(input, Pair(currentRow, currentColumn), nextDirectionIdx)
         }
-
-        return rotatedInput
-    }
-
-    fun transformPosition(numCols: Int, currentPosition: Pair<Int, Int>): Pair<Int, Int> {
-        val (row, col) = currentPosition
-        return Pair(numCols - col - 1, row)
+        return traverseGrid(input, Pair(nextRow, nextColumn), currentDirectionIdx)
     }
 
     fun isValidPosition(input: List<MutableList<Char>>, row: Int, col: Int): Boolean {
-        var nextInput = input.map { it.toMutableList() }
-        nextInput[row][col] = '#'
-        val currentPosition = findInitialPosition(nextInput)
-        var nextPosition = findNextPosition(nextInput, currentPosition)
-
-        val directions = listOf('U', 'R', 'D', 'L')
-        var currentDirectionIdx = 0
-
-        while (nextPosition.first != 0) {
-            val (obstaclePositionRow, obstaclePositionCol) = Pair(nextPosition.first - 1, nextPosition.second)
-            if (nextInput[obstaclePositionRow][obstaclePositionCol] == directions[currentDirectionIdx]) {
-                return true
-            }
-            nextInput[obstaclePositionRow][obstaclePositionCol] = directions[currentDirectionIdx]
-            currentDirectionIdx = (currentDirectionIdx + 1) % 4
-            nextInput = rotateInput(nextInput)
-            nextPosition = findNextPosition(nextInput, transformPosition(nextInput[0].size, nextPosition))
-        }
-
-        return false
+        var trialInput = input.map { it.toMutableList() }
+        trialInput[row][col] = '#'
+        val currentPosition = findInitialPosition(trialInput)
+        val currentDirectionIdx = 0
+        return traverseGrid(trialInput, currentPosition, currentDirectionIdx)
     }
 
     fun part1(input: List<MutableList<Char>>): Int {
-        var nextInput = input.map { it.toMutableList() }
-        val currentPosition = findInitialPosition(nextInput)
-        var nextPosition = findNextPosition(nextInput, currentPosition)
-        while (nextPosition.first != 0) {
-            nextInput = rotateInput(nextInput)
-            nextPosition = findNextPosition(nextInput, transformPosition(nextInput[0].size, nextPosition))
-        }
-        return nextInput.sumOf { it.count { char -> char == 'X' } }
+        var input = input
+        val currentPosition = findInitialPosition(input)
+        val currentDirectionIdx = 0
+        traverseGrid(input, currentPosition, currentDirectionIdx)
+        return input.sumOf { it.count { char -> char == 'X' } }
     }
 
     fun part2(input: List<MutableList<Char>>): Int {
+        var input = input
+        val inputWithX = input.map { it.toMutableList() }
+        part1(inputWithX)
         var numPositions = 0
         for (row in input.indices) {
             for (col in input[0].indices) {
-                if (input[row][col] == '.' && isValidPosition(input, row, col)) {
+                // Only check for positions visited in part 1
+                if (inputWithX[row][col] == 'X' && input[row][col] != '^' && isValidPosition(input, row, col)) {
                     numPositions++
                 }
             }
@@ -102,7 +80,8 @@ fun main() {
         return numPositions
     }
 
-    val input = readInput("Day06").map { it.toMutableList() }
-    println(part1(input))
-    println(part2(input))
+    val inputPart1 = readInput("Day06").map { it.toMutableList() }
+    println(part1(inputPart1))
+    val inputPart2 = readInput("Day06").map { it.toMutableList() }
+    println(part2(inputPart2))
 }
